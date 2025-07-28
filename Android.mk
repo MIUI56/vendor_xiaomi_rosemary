5dd5b62a0685678021b00bca119ed4d9dc2e5fbc
miui56 <payush706@gmail.com>
2025-07-28 09:50:13 +0000

    rosemary:resync with dt

--- Android.mk
+++ Android.mk
@@ -1,22 +1,3 @@
-#
-# Automatically generated file. DO NOT MODIFY
-#
-
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),rosemary)
-
-$(call add-radio-file-sha1-checked,radio/audio_dsp.img,dbc6ae6e7109ec4ca98da9dd15bcab1a0d38d9a1)
-$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,217ec73ccd355f69ba22cf99d9de18bce715f75f)
-$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,c71ab5f25b9b44a943a199262af4c89847ed31bc)
-$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,6d300741d92c2f7c7d76a211a054c38162f6ee71)
-$(call add-radio-file-sha1-checked,radio/gz.img,5afff9eefa64c2e24dc794ee8d5956d2594b78da)
-$(call add-radio-file-sha1-checked,radio/lk.img,6311dcbfe0fe22e3149c828d4ba61e6a6a917864)
-$(call add-radio-file-sha1-checked,radio/md1img.img,06ffb17f82950ca938b3bf04a3907ea8bc9face6)
-$(call add-radio-file-sha1-checked,radio/preloader_raw.img,c963964e230ef975003fd1f790fe8952f18baa1b)
-$(call add-radio-file-sha1-checked,radio/scp.img,d795b777756f0665b9523dcf3fb6118db80c3289)
-$(call add-radio-file-sha1-checked,radio/spmfw.img,c5f1a5b624a1c94459b6ca46d296e3a54d8ab700)
-$(call add-radio-file-sha1-checked,radio/sspm.img,c8e1ce7c455b714203f3de9a75fdaa34c3ada22d)
-$(call add-radio-file-sha1-checked,radio/tee.img,45d71e28ce486e230440b12686a37408bac337b7)
-
-endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:b10eac8388b148e55c00354209574e587d7a997a7b1bf7e190078717c83eee52
+size 1249
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,17 +1,3 @@
-#
-# Automatically generated file. DO NOT MODIFY
-#
-
-AB_OTA_PARTITIONS += \
-    audio_dsp \
-    cam_vpu1 \
-    cam_vpu2 \
-    cam_vpu3 \
-    gz \
-    lk \
-    md1img \
-    preloader_raw \
-    scp \
-    spmfw \
-    sspm \
-    tee
+version https://git-lfs.github.com/spec/v1
+oid sha256:becdb84164e8840c440a36e2f4c3b764fd5e69b1954fb7fc2bc870dd82ae2392
+size 227
